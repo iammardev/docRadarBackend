9eb3a885d6fe074bc80dd8a6aedf40d51a6925b6
Ammar Asif <64131345+iammardev@users.noreply.github.com>
2025-04-14 02:12:31 +0500

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ const allowedOrigins = [
   "http://localhost:5174",
   "http://localhost:3000",
   "http://localhost:58559",
-  "http://localhost:55444",
+  "http://localhost:51907",
 ];
 
 app.use(
