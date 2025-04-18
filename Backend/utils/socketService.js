4a4bd652d898401eb60b151169a6bc8448eb17ee
Ammar Asif <64131345+iammardev@users.noreply.github.com>
2025-04-18 21:56:37 +0500

    Update socketService.js

--- Backend/utils/socketService.js
+++ Backend/utils/socketService.js
@@ -12,7 +12,7 @@ const pendingRequests = new Map(); // requestId -> { userId, doctorIds[], accept
 export const initializeSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: ["http://localhost:3000","http://localhost:5174" , "http://localhost:5173"],
+      origin: ["http://localhost:3000","http://localhost:5174" , "http://localhost:5173","http://localhost:8080"],
       methods: ["GET", "POST"],
       credentials: true,
     },
